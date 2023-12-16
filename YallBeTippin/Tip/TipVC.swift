293879d095f68a42ffdd6486e0017e719eadcc71
R K <kanno.media808@gmail.com>
2023-12-15 23:47:40 -0800

    Add initializer.

--- YallBeTippin/Tip/TipVC.swift
+++ YallBeTippin/Tip/TipVC.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class TipVC: UIViewController {
     static let id = "TipVC"
-
+    
+    let orderItems: [MenuItem]
+    
+    init(coder: NSCoder, orderItems: [MenuItem]) {
+        self.orderItems = orderItems
+        super.init(coder: coder)!
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        title = "Just gonna asking a question"
         
     }
 }
